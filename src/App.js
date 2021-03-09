420d13e5b4243188a897aefb902933390573b65c
Shujaat Azim <shujaat.azim@gmail.com>
2021-03-08 20:03:49 -0500

    Built out some of the app and the css parts, but kinda annoyed that the tutorial is giving the code with little explanation.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="topheader">
+        <header className="container">
+          <nav className="navbar">
+            <div className="navbar-brand">
+                <span className="navbar-item">Shtox</span>
+            </div>
+            <div className="navbar-end">
+                <a className="navbar-item" href="/" target="_blank" rel="noopener noreferrer">Shtox.com</a>
+            </div>
+          </nav>
+        </header>
+      </div>
+      <section className="results--section">
+        <div className="container">
+            <h1>Shtox shows realtime price information about<br></br> BTC, ETH and LTC (will add more later).</h1>
+        </div>
+        <div className="results--section__inner">
+            {/* <Today />
+            <History /> */}
+        </div>
+      </section>
     </div>
   );
 }
